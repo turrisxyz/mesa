6712a2ad442be640efda7dab9aa29e621092a6f1
Marcin Ślusarz <marcin.slusarz@intel.com>
2020-11-12 20:13:26 +0100

    nir: handle float atomics in nir_lower_memory_model
    Signed-off-by: Marcin Ślusarz <marcin.slusarz@intel.com>
    Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
    Fixes: 2adb337256f ("nir,radv/aco: add and use pass to lower make available/visible barriers")
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10766>
    (cherry picked from commit 2c3e2d69bd1e1ab715783f0ef41daf99519ed12e)

--- src/compiler/nir/nir_lower_memory_model.c
+++ src/compiler/nir/nir_lower_memory_model.c
@@ -76,6 +76,10 @@ get_intrinsic_info(nir_intrinsic_instr *intrin, nir_variable_mode *modes,
    case nir_intrinsic_ssbo_atomic_xor:
    case nir_intrinsic_ssbo_atomic_exchange:
    case nir_intrinsic_ssbo_atomic_comp_swap:
+   case nir_intrinsic_ssbo_atomic_fadd:
+   case nir_intrinsic_ssbo_atomic_fcomp_swap:
+   case nir_intrinsic_ssbo_atomic_fmax:
+   case nir_intrinsic_ssbo_atomic_fmin:
       *modes = nir_var_mem_ssbo;
       *reads = true;
       *writes = true;
@@ -98,6 +102,10 @@ get_intrinsic_info(nir_intrinsic_instr *intrin, nir_variable_mode *modes,
    case nir_intrinsic_global_atomic_xor:
    case nir_intrinsic_global_atomic_exchange:
    case nir_intrinsic_global_atomic_comp_swap:
+   case nir_intrinsic_global_atomic_fadd:
+   case nir_intrinsic_global_atomic_fcomp_swap:
+   case nir_intrinsic_global_atomic_fmax:
+   case nir_intrinsic_global_atomic_fmin:
       *modes = nir_var_mem_global;
       *reads = true;
       *writes = true;
@@ -120,6 +128,10 @@ get_intrinsic_info(nir_intrinsic_instr *intrin, nir_variable_mode *modes,
    case nir_intrinsic_deref_atomic_xor:
    case nir_intrinsic_deref_atomic_exchange:
    case nir_intrinsic_deref_atomic_comp_swap:
+   case nir_intrinsic_deref_atomic_fadd:
+   case nir_intrinsic_deref_atomic_fmin:
+   case nir_intrinsic_deref_atomic_fmax:
+   case nir_intrinsic_deref_atomic_fcomp_swap:
       *modes = nir_src_as_deref(intrin->src[0])->modes;
       *reads = true;
       *writes = true;
