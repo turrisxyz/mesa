176213f01c59ca8268a91f0e84306cc2a17984ef
Dave Airlie <airlied@redhat.com>
2021-05-07 11:26:35 +1000

    gallivm: handle texture arrays in non-fragment shaders with lod.
    We have to unwind the lod into the scalar path correctly.
    
    Fixes a crash with renderdoc demo
    
    Fixes: e168d148d76d ("gallivm/nir: handle non-uniform texture offsets")
    Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10676>
    (cherry picked from commit 83a05caaf2ce4fa9d6605eddd5658bf400f642fb)

--- src/gallium/auxiliary/gallivm/lp_bld_nir_soa.c
+++ src/gallium/auxiliary/gallivm/lp_bld_nir_soa.c
@@ -1394,10 +1394,10 @@ static void emit_tex(struct lp_build_nir_context *bld_base,
                                  LLVMGetUndef(bld_base->base.vec_type),
                                  LLVMGetUndef(bld_base->base.vec_type),
                                  LLVMGetUndef(bld_base->base.vec_type) };
-      LLVMValueRef texel[4], orig_offset;
+      LLVMValueRef texel[4], orig_offset, orig_lod;
       unsigned i;
       orig_texel_ptr = params->texel;
-
+      orig_lod = params->lod;
       for (i = 0; i < 5; i++) {
          coords[i] = params->coords[i];
       }
@@ -1416,6 +1416,8 @@ static void emit_tex(struct lp_build_nir_context *bld_base,
                                                                 idx, "");
          params->type = lp_elem_type(bld_base->base.type);
 
+         if (orig_lod)
+            params->lod = LLVMBuildExtractElement(gallivm->builder, orig_lod, idx, "");
          params->texel = texel;
          bld->sampler->emit_tex_sample(bld->sampler,
                                        gallivm,
